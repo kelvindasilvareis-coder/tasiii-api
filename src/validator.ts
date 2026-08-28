26f902dceee2c317d6dc9e4a966d05a99cbf5be7
Kelvin <kelvindasilvareis@gmail.com>
2026-08-28 20:53:31 -0300

    ajustando o erro

--- src/validator.ts
+++ src/validator.ts
@@ -2,7 +2,16 @@ function validarChamado(titulo: string): boolean {
     if (!titulo || !titulo.trim()) {
         throw new Error("O título do chamado não pode ser vazio.");
     }
+    
+    const tituloLimpo = titulo.trim();
 
+    if (tituloLimpo.length < 5) {
+        throw new Error("O título do chamado deve possuir no mínimo 5 caracteres.");
+    }
+
+    if (tituloLimpo.length > 100) {
+        throw new Error("O título do chamado deve possuir no máximo 100 caracteres.");
+    }
 
     return true;
 }
